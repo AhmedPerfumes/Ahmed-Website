4527355dfbbfbdedfee411505727a299ef78e307
Saad Edroos <saadedroossaad@gmail.com>
2024-08-06 12:36:57 +0400

    Fixed popup issue

--- components/homes/home-3/Categories.jsx
+++ components/homes/home-3/Categories.jsx
@@ -2,7 +2,6 @@
 import { categories2 } from "@/data/categories";
 import { Autoplay, Navigation } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
-import Image from "next/image";
 import React, { useEffect } from "react";
 
 export default function Categories() {
@@ -16,10 +15,13 @@ export default function Categories() {
       tooltipVideo.load();
       tooltip.classList.add("show");
       tooltip.style.display = "block";
+      const rect = e.target.getBoundingClientRect();
+      tooltip.style.left = `${rect.left}px`;
+      tooltip.style.top = `${rect.top - tooltip.offsetHeight}px`;
     }
   };
 
-  const handleMouseOut = (e) => {
+  const handleMouseOut = () => {
     const tooltip = document.getElementById("video-tooltip");
 
     if (tooltip) {
@@ -27,15 +29,6 @@ export default function Categories() {
       tooltip.style.display = "none";
     }
   };
-
-  document.addEventListener("mousemove", (e) => {
-    const tooltip = document.getElementById("video-tooltip");
-    if (tooltip && tooltip.classList.contains("show")) {
-      tooltip.style.left = `${e.pageX}px`;
-    }
-  });
-
-  // -----------
 
   const swiperOptions = {
     autoplay: {
@@ -54,76 +47,30 @@ export default function Categories() {
       320: {
         slidesPerView: 4,
         slidesPerGroup: 1,
-        // spaceBetween: 15,
         pagination: false,
       },
       768: {
         slidesPerView: 4,
         slidesPerGroup: 1,
-        // spaceBetween: 30,
         pagination: false,
       },
       992: {
         slidesPerView: 4,
         slidesPerGroup: 1,
-        // spaceBetween: 45,
         pagination: false,
       },
       1200: {
         slidesPerView: 5,
         slidesPerGroup: 0,
-        // spaceBetween: 60,
         pagination: false,
       },
     },
   };
+
   return (
     <section className="category-carousel container">
       <div className="position-relative">
-        {/* <Swiper
-          {...swiperOptions}
-          className="swiper-container js-swiper-slider"
-        >
-          {categories2.map((elm, i) => (
-            <SwiperSlide key={i} className="swiper-slide text-center">
-              <a
-                key={i}
-                href="/shop-8?category=eau-de-parfum&subcategory=oriental-fragrance"
-                className="shop-categories__item mb-3"
-              >
-                <video
-                  loading="lazy"
-                  // src={elm.imgSrc}
-                  width="200"
-                  height="120"
-                  alt=""
-                  className="shop-categories__item-img rounded-circle text-center"
-                  autoPlay
-                  loop
-                  muted
-                >
-                  <source src={elm.videoSrc} type="video/mp4" width={200} />
-                </video>
-              </a>
-              <div className="text-center">
-                <a
-                  href="/shop-8?category=eau-de-parfum&subcategory=oriental-fragrance"
-                  className="menu-link fw-medium"
-                  key={i}
-                >
-                  {elm.category.split(" ")[0]}
-                  <br />
-                  {elm.category.split(" ")[1]}
-                </a>
-              </div>
-            </SwiperSlide>
-          ))}
-        </Swiper> */}
-
-        <Swiper
-          {...swiperOptions}
-          className="swiper-container js-swiper-slider"
-        >
+        <Swiper {...swiperOptions} className="swiper-container js-swiper-slider">
           {categories2.map((elm, i) => (
             <SwiperSlide key={i} className="swiper-slide text-center">
               <a
@@ -141,7 +88,7 @@ export default function Categories() {
                   muted
                   data-video-src={elm.videoSrc}
                   onMouseOver={(e) => handleMouseOver(e)}
-                  onMouseOut={(e) => handleMouseOut(e)}
+                  onMouseOut={handleMouseOut}
                 >
                   <source src={elm.videoSrc} type="video/mp4" width={200} />
                 </video>
