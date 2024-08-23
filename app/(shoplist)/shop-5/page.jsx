61cf89055ccf67c50a036ff48659eb5997ee3491
Yazil Ansari <yazilansari@gmail.com>
2024-08-23 17:01:54 +0400

    Changed Title and Logo

--- app/(shoplist)/shop-5/page.jsx
+++ app/(shoplist)/shop-5/page.jsx
@@ -6,8 +6,11 @@ import Header14 from "@/components/headers/Header14";
 import Shop5 from "@/components/shoplist/Shop5";
 
 export const metadata = {
-  title: "Shop 5 || Uomo eCommerce React Nextjs Template",
-  description: "Uomo eCommerce React Nextjs Template",
+  title: "Perfumes | Buy Best Perfumes Online | Ahmed Perfume",
+  description: "Buy Best Perfumes Online Ahmed Perfume",
+  icons: {
+    icon: 'https://www.ahmed-perfume.com/wp-content/uploads/2021/08/Ahmed-Logo-e1631552829722-100x100.png',
+  },
 };
 export default function ShopPage5() {
   return (
