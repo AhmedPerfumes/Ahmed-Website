e691d32b880e451a055423471fa0bc5f555a9aa2
Saad Edroos <saadedroossaad@gmail.com>
2024-08-29 11:09:02 +0400

    Fixed glitches for different pages

--- app/(shoplist)/shop-5/page.jsx
+++ app/(shoplist)/shop-5/page.jsx
@@ -26,7 +26,7 @@ export default function ShopPage5() {
         <Footer14 />
         
       </section>
-      <section className="testsect d-sm-block d-md-none bg-dark pt-5  ">
+      <section className=" d-sm-block d-md-none bg-dark pt-5  ">
       <div className="MobileFooter">
       
 
