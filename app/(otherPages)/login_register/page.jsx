686480bf0ad8872507af21be6297613e0bc3dd57
Saad Edroos <saadedroossaad@gmail.com>
2024-09-11 15:46:40 +0400

    Fixed metadata in login page

--- app/(otherPages)/login_register/page.jsx
+++ app/(otherPages)/login_register/page.jsx
@@ -5,8 +5,11 @@ import LoginRegister from "@/components/otherPages/LoginRegister";
 import React from "react";
 
 export const metadata = {
-  title: "Login Register || Uomo eCommerce React Nextjs Template",
-  description: "Uomo eCommerce React Nextjs Template",
+  title: "Perfumes | Buy Best Perfumes Online | Ahmed Perfume",
+  description: "Buy Best Perfumes Online Ahmed Perfume",
+  icons: {
+    icon: 'https://www.ahmedalmaghribi.com/wp-content/uploads/2021/08/Ahmed-Logo-e1631552829722-100x100.png',
+  },
 };
 export default function LoginPage() {
   return (
