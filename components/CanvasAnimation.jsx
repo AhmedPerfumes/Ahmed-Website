7e09f24266d42eb0bfba3a541f9b4c8b857e9be8
Saad Edroos <saadedroossaad@gmail.com>
2024-10-16 12:17:23 +0400

    Remove snap effect

--- components/CanvasAnimation.jsx
+++ components/CanvasAnimation.jsx
@@ -47,14 +47,14 @@ const CanvasAnimation = () => {
         scrub: 1,
         pin: canvas,
         end: '250%',
-        onLeave: () => {
-          // Snap to the next section when the last frame is reached
-          gsap.to(window, {
-            scrollTo: { y: "#animation-section", autoKill: false }, // Smooth scroll to the section
-            duration: 0.5,
-            ease: "power2.inOut",
-          });
-        },
+        // onLeave: () => {
+        //   // Snap to the next section when the last frame is reached
+        //   gsap.to(window, {
+        //     scrollTo: { y: "#animation-section", autoKill: false }, // Smooth scroll to the section
+        //     duration: 0.5,
+        //     ease: "power2.inOut",
+        //   });
+        // },
       },
       onUpdate: render,
     });
