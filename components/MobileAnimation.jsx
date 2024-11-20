e77b48669229a825c59695a9869177e2a06f70e0
Yazil Ansari <yazilansari@gmail.com>
2024-11-20 11:46:05 +0400

    Optimize Home Page Loader

--- components/MobileAnimation.jsx
+++ components/MobileAnimation.jsx
@@ -108,7 +108,7 @@ const CanvasAnimation = () => {
   }, []); // Run effect once after component mounts
 
   useEffect(() => {
-    if (loadingProgress === 90) {
+    if (loadingProgress <= 90) {
       setTimeout(() => {
         const loadingScreen = document.querySelector(".loading-screen");
         
