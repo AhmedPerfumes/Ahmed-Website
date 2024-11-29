f4e907d347197e11042c96951bce5a3d0bacf4c8
Yazil Ansari <yazilansari@gmail.com>
2024-11-29 11:02:40 +0400

    Added Image Frames

--- components/CanvasAnimation.jsx
+++ components/CanvasAnimation.jsx
@@ -16,11 +16,7 @@ const CanvasAnimation = () => {
   }, []);
 
   return (
-    !isMobile ?
-      <section className="sectionWebMob">
-        <div scroll-frames="web" data-url-mask="/assets/final/|1 to 343|.jpg" data-background-size="cover" data-detector="the_detector_web"></div>
-        <hr id="the_detector_web"/>
-      </section> : 
+    
       <section className="sectionWebMob">
         <div scroll-frames="mobile" data-url-mask="/assets/mobilescreencomp/|1 to 140|.jpg" data-background-size="cover" data-detector="the_detector_mob"></div>
         <hr id="the_detector_mob"/>
