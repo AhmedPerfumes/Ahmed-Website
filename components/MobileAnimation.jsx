6d112c90fde9fe4f58263e7bb07f3ef299e578ee
Yazil Ansari <yazilansari@gmail.com>
2024-11-29 17:27:42 +0400

    Added Image Frames

--- components/MobileAnimation.jsx
+++ components/MobileAnimation.jsx
@@ -1,16 +1,69 @@
 "use client";
-
+import { useEffect, useRef, useState } from "react";
 import "./Canvas.css";
 
 const MobileAnimation = () => {
+  const previousScrollY = useRef(0); // Store previous scroll position for comparison
+  const [showSkipButton, setShowSkipButton] = useState(true);
+  const handleScroll = () => {
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    const totalHeight = document.documentElement.scrollHeight - window.innerHeight;
+    const scrollPercentage = scrollTop / totalHeight;  // Get the scroll percentage (0 to 1)
+
+    // Determine the image based on scroll percentage
+    const totalImages = 136; // Total number of images (adjust based on your data)
+    const imageIndex = Math.floor(scrollPercentage * totalImages);  // Calculate the image index
+
+    // Update the image source
+    console.log(imageIndex);
+
+    // Auto-scroll to the next section when imageIndex reaches 9
+    const currentScrollY = window.scrollY;
+    if(currentScrollY > previousScrollY.current) {
+      if (imageIndex == 9) {
+        // Scroll to the next section with a smooth scroll
+        const nextSection = document.getElementById("main2");
+        if (nextSection) {
+          nextSection.scrollIntoView({ behavior: "smooth" });
+        }
+      }
+    }
+    previousScrollY.current = currentScrollY;
+  };
+  const skipAnimation = () => {
+    const nextSection = document.getElementById("main2");
+    if (nextSection) {
+      nextSection.scrollIntoView({ behavior: "smooth" });
+      setShowSkipButton(false);
+    }
+  };
+  useEffect(() => {
+      // Use setTimeout to ensure page rendering is complete before scrolling
+      setTimeout(() => {
+        window.scrollTo(0, 0);
+      }, 0);  // Delay the scroll action to ensure it's executed after render
+
+      // Attach the scroll event listener
+      window.addEventListener("scroll", handleScroll);
+
+      // Cleanup the event listener when the component is unmounted
+      return () => {
+        window.removeEventListener("scroll", handleScroll);
+      };
+  }, []);
   return (
     <section className="sectionWebMob">
       <div scroll-frames="demo"
-          data-url-mask="/assets/mobilescreencomp/|1 to 140|.jpg"
+          data-url-mask="/assets/mobilescreencomp/|1 to 136|.jpg"
           data-background-size="cover"
           data-detector="the_detector">
       </div>
       <hr id="the_detector"/>
+      {showSkipButton && (
+        <button onClick={skipAnimation} className="skip-button">
+          SKIP INTRO
+        </button>
+      )}
     </section>
   );
 };
