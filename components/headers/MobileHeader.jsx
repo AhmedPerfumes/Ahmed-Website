cb720b2d23deed680230bee93ccf3c240b023e9e
Saad Edroos <saadedroossaad@gmail.com>
2024-12-12 09:27:36 +0400

    Fixed country options for Iphone

--- components/headers/MobileHeader.jsx
+++ components/headers/MobileHeader.jsx
@@ -162,7 +162,7 @@ export default function MobileHeader() {
         </div>
         {/* <!-- /.container --> */}
 
-        <div className="border-top mt-auto pb-2">
+        <div className="border-top mt-3 pb-2">
           <div className="customer-links container mt-4 mb-2 pb-1">
             <svg
               className="d-inline-block align-middle"
