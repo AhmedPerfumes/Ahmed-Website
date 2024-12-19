8e10880c534e27a5493a392becab77a29ed4e231
Saad Edroos <saadedroossaad@gmail.com>
2024-12-19 15:30:48 +0400

    Fixed discount text

--- components/modals/QuickView.jsx
+++ components/modals/QuickView.jsx
@@ -100,7 +100,7 @@ export default function QuickView() {
     if(elm?.discount) {
       // console.log(current_date_time, new Date(elm.discount.start_date), new Date(elm.discount.end_date));
       if(new Date(current_date_time) >= new Date(elm.discount.start_date) && new Date(current_date_time) <= new Date(elm.discount.end_date)) {
-        return <><span className="money price price-old">{elm?.price}د.إ</span> <span className="money price price-sale"> {(elm.price - (elm.price / 100 * elm.discount.value)).toFixed(2)}د.إ</span></>;
+        return <><span className="money price price-old">د.إ{elm?.price}</span> <span className="money price price-sale"> د.إ{(elm.price - (elm.price / 100 * elm.discount.value)).toFixed(2)}</span></>;
       } else {
         return <span className="money price">{elm?.price}د.إ</span>;
       }
