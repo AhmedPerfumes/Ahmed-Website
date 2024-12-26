b271688b7a7c84add9314c827699918e556d6bed
Saad Edroos <saadedroossaad@gmail.com>
2024-12-26 14:39:31 +0400

    Created city walk page

--- components/campagin/Citywalk.jsx
+++ components/campagin/Citywalk.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Hero from '../homes/home-5/Hero'
+import Hero from '../homes/home-2/Hero'
 import Image from 'next/image'
 import Link from 'next/link'
 import ContactForm from '../otherPages/Contact/ContactForm'
@@ -7,7 +7,26 @@ import ContactForm from '../otherPages/Contact/ContactForm'
 function CityWalk() {
   return (
     <div>
-        <Hero/>
+       <div className="container-fluid p-0 pt-2">
+        <Image
+          loading="lazy"
+          className="w-100 h-auto d-none d-lg-block"
+          src="/assets/images/home/demo5/city-walk.jpg"
+          alt="image"
+          width={1500}
+          height={550}
+        />
+      </div>
+      <div className="container-fluid p-0 pt-2">
+        <Image
+          loading="lazy"
+          className="w-100 h-auto d-lg-none"
+          src="/assets/images/home/demo5/mobile-city.jpg"
+          alt="image"
+          width={1500}
+          height={550}
+        />
+      </div>
         <ContactForm/>
         
     </div>
