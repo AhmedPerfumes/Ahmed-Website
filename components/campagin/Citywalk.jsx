31c744f847288eddcd52864d56842f0c9f956d2f
Saad Edroos <saadedroossaad@gmail.com>
2024-12-26 10:43:50 +0400

    Added city walk campaign page

---
+++ components/campagin/Citywalk.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Hero from '../homes/home-5/Hero'
+import Image from 'next/image'
+import Link from 'next/link'
+import ContactForm from '../otherPages/Contact/ContactForm'
+
+function CityWalk() {
+  return (
+    <div>
+        <Hero/>
+        <ContactForm/>
+        
+    </div>
+  )
+}
+
+export default CityWalk
