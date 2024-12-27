1dcef677698ac092f68576ff0153ca1f88d9ad6c
“Arshad <arshad@ahmedalmaghribi.com>
2024-12-27 17:02:05 +0400

    resolved homepage issue

--- app/[locale]/(homes)/home-8/page.jsx
+++ app/[locale]/(homes)/home-8/page.jsx
@@ -1,6 +1,6 @@
 import Footer14 from "@/components/footers/Footer14";
 import Header14 from "@/components/headers/Header14";
-import Homepage from "@/components/Homepage";
+import Animation from "@/components/Animation";
 import MobileFooter2 from "@/components/footers/MobileFooter2";
 import React from "react";
 import CanvasAnimation from "@/components/CanvasAnimation";
@@ -14,35 +14,42 @@ import NewsLetter from "@/components/modals/NewsLetter";
 // });
 
 export const metadata = {
-  title: "Perfumes | Buy Best Perfumes Online | Ahmed Perfume",
-  description: "Buy Best Perfumes Online Ahmed Perfume",
-  icons: {
-    icon: "https://www.ahmedalmaghribi.com/wp-content/uploads/2021/08/Ahmed-Logo-e1631552829722-100x100.png",
-  },
+    title: "Perfumes | Buy Best Perfumes Online | Ahmed Perfume",
+    description: "Buy Best Perfumes Online Ahmed Perfume",
+    icons: {
+        icon: "https://www.ahmedalmaghribi.com/wp-content/uploads/2021/08/Ahmed-Logo-e1631552829722-100x100.png",
+    },
 };
 
 export default function HomePage8() {
-  return (
-    <div
-      style={{
-        backgroundImage: `url(/assets/background-ivory.webp)`,
-      }}
-    >
-      {/* <NewsLetter /> */}
-      {/* <Header14 /> */}
-
-      <main id="">
-        {/* Animation component will render after CanvasAnimation */}
-        <Homepage />
-      </main>
-      <section className="d-none d-lg-block" style={{ height: "100%" }}>
-        <Footer14 />
-      </section>
-      <section className="d-sm-block d-md-none bg-dark pt-5">
-        <div className="MobileFooter">
-          <MobileFooter2 />
+    return (
+        <div
+            style={{
+                backgroundImage: `url(/assets/background-ivory.webp)`,
+            }}
+        >
+            <NewsLetter />
+            <Header14 />
+            <div className="d-none d-lg-block">
+                {/* CanvasAnimation will render first for large screens */}
+                <CanvasAnimation />
+            </div>
+            <div className="d-sm-block d-md-none">
+                {/* MobileAnimation will render for small screens */}
+                <MobileAnimation />
+            </div>
+            <main id="">
+                {/* Animation component will render after CanvasAnimation */}
+                <Animation />
+            </main>
+            <section className="d-none d-lg-block" style={{ height: "100%" }}>
+                <Footer14 />
+            </section>
+            <section className="d-sm-block d-md-none bg-dark pt-5">
+                <div className="MobileFooter">
+                    <MobileFooter2 />
+                </div>
+            </section>
         </div>
-      </section>
-    </div>
-  );
+    );
 }
