f1b27c86fe069df50b96a0e2fbf5ba4c1665c960
Saad Edroos <saadedroossaad@gmail.com>
2025-01-15 16:30:21 +0400

    Translated

--- components/Metapixel.jsx
+++ components/Metapixel.jsx
@@ -16,8 +16,8 @@ export const FacebookPixelEvents = () => {
         // Example: Track a conversion event with a token
         const conversionToken = "EAADhP2FyWZAMBO6CAWMq5YxAjtKauCOOulAhpS3ZAvDJ2NP7XXUrOhR9u7ZCzZComTVtFxVFM6c2SZA0yAztBPLoX5UqecbwC3Kss4tvlk0wvMcp4Fx45tT5838bQgO8HUWd6z6It4zZA8uxPNiBzvNLlXkW9rg55Kv5GraBn2sxaN8kzFuHBICRbooLPAY1xyQAZDZD"; // Replace with your dynamic token logic
 
-        ReactPixel.track("Lead", {
-          currency: "USD", // Adjust as needed
+        ReactPixel.track("Checkout", {
+          currency: "AED", // Adjust as needed
           value: 100.0, // Adjust the value as appropriate for your use case
           token: conversionToken, // Add the token
         });
