80899ab4ddf9d032bda77846a3093d0561cccd6e
Yazil Ansari <yazilansari@gmail.com>
2025-01-15 16:39:00 +0400

    Added Pixel Component

--- components/Metapixel.jsx
+++ components/Metapixel.jsx
@@ -14,13 +14,13 @@ export const FacebookPixelEvents = () => {
         ReactPixel.pageView();
 
         // Example: Track a conversion event with a token
-        const conversionToken = "EAADhP2FyWZAMBO6CAWMq5YxAjtKauCOOulAhpS3ZAvDJ2NP7XXUrOhR9u7ZCzZComTVtFxVFM6c2SZA0yAztBPLoX5UqecbwC3Kss4tvlk0wvMcp4Fx45tT5838bQgO8HUWd6z6It4zZA8uxPNiBzvNLlXkW9rg55Kv5GraBn2sxaN8kzFuHBICRbooLPAY1xyQAZDZD"; // Replace with your dynamic token logic
+        // const conversionToken = "EAADhP2FyWZAMBO6CAWMq5YxAjtKauCOOulAhpS3ZAvDJ2NP7XXUrOhR9u7ZCzZComTVtFxVFM6c2SZA0yAztBPLoX5UqecbwC3Kss4tvlk0wvMcp4Fx45tT5838bQgO8HUWd6z6It4zZA8uxPNiBzvNLlXkW9rg55Kv5GraBn2sxaN8kzFuHBICRbooLPAY1xyQAZDZD"; // Replace with your dynamic token logic
 
-        ReactPixel.track("Lead", {
-          currency: "USD", // Adjust as needed
-          value: 100.0, // Adjust the value as appropriate for your use case
-          token: conversionToken, // Add the token
-        });
+        // ReactPixel.track("Lead", {
+        //   currency: "AED", // Adjust as needed
+        //   value: '100.0', // Adjust the value as appropriate for your use case
+        //   token: conversionToken, // Add the token
+        // });
       });
   }, [pathname, searchParams]);
 
