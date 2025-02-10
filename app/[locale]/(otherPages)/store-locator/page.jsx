8937bccecb8f8faf13a6860be447e8b7bef179d2
Yazil Ansari <yazilansari@gmail.com>
2025-02-10 11:16:51 +0400

    Changed New Banner Link

---
+++ app/[locale]/(otherPages)/store-locator/page.jsx
@@ -0,0 +1,25 @@
+import Footer1 from "@/components/footers/Footer1";
+
+import Header14 from "@/components/headers/Header14";
+
+import StoreLocator from "@/components/otherPages/StoreLocator";
+import React from "react";
+
+export const metadata = {
+  title: "Store Location || Uomo eCommerce React Nextjs Template",
+  description: "Uomo eCommerce React Nextjs Template",
+};
+export default function StoreLocationPage() {
+  return (
+    <>
+      <Header14 />
+      <main className="page-wrapper">
+        <div className="mb-4 pb-4"></div>
+        <StoreLocator />
+      </main>
+
+      <div className="mb-5 pb-xl-5"></div>
+      <Footer1 />
+    </>
+  );
+}
