51b31f2d40a56ee982b8726e8808d997e3be4a04
“Arshad <arshad@ahmedalmaghribi.com>
2025-06-11 18:58:24 +0400

    edited LP

--- components/homes/home-15/Categories.jsx
+++ components/homes/home-15/Categories.jsx
@@ -156,7 +156,7 @@ export default function Categories({ section }) {
                 </h2>
                 <p className="fs-15 mb-4 pb-xl-2 mb-xl-4 text-secondary text-center section-paragraph">
                     {t(
-                        "Timeless Perfumes for Dad"
+                        "A special line just for Dads"
                     )}
                 </p>
                 <Swiper className="swiper-container" {...swiperOptions}>
