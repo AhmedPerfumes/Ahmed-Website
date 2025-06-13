2c6a2f0b19d21b04e8c1314d8523b61ac1698c9d
Yazil Ansari <yazilansari@gmail.com>
2025-06-13 16:04:54 +0400

    Added Caching for videos

--- next.config.mjs
+++ next.config.mjs
@@ -20,7 +20,20 @@ const nextConfig = {
           }
         ],
       },
-      productionBrowserSourceMaps: true
+      productionBrowserSourceMaps: true,
+      async headers() {
+      return [
+        {
+          source: '/assets/videos/:path*',
+          headers: [
+            {
+              key: 'Cache-Control',
+              value: 'public, max-age=31536000, immutable',
+            },
+          ],
+        },
+      ];
+    },
 };
 
 export default withNextIntl(nextConfig);
