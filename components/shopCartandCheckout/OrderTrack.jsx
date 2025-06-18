1f05513258f171b8aa744987e2e665e2cc711845
Saad Edroos <saadedroossaad@gmail.com>
2025-06-18 10:20:20 +0400

    Added on trackpage

--- components/shopCartandCheckout/OrderTrack.jsx
+++ components/shopCartandCheckout/OrderTrack.jsx
@@ -231,13 +231,14 @@ export default function OrderTrack() {
                   <th>SERVICE FEE { orderDetails.payment_method === "cod" && '(Including COD Charges)'}</th>
                   <td>{ (orderDetails.service_amount * 1.05).toFixed(2) }{ currency.symbol }</td> 
                 </tr>
-                <tr>
                 { orderDetails.payment_method === "cod" && (
                                                     <tr>
                                                         <th>COD Charges</th>
                                                         <td>10.00</td>
                                                     </tr>
                                                 )}
+                <tr>
+                
                   <th>TOTAL</th>
                   <td>{orderDetails.total}{ currency.symbol } (includes { orderDetails.tax_amount }{ currency.symbol } VAT)
                   </td>
