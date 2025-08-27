f03adeedeb669a38e73bd13adc699ab9b14c38d0
Saad Edroos <saadedroossaad@gmail.com>
2025-08-27 12:47:40 +0400

    Adde new launch page

--- app/[locale]/(otherPages)/new_launch/page.jsx
+++ app/[locale]/(otherPages)/new_launch/page.jsx
@@ -4,7 +4,17 @@ import React from 'react'
 function page() {
   return (
     <div>
+        <Header14 />
         <NewLaunch/>
+         <section className="d-none d-lg-block" style={{ height: "100%" }}>
+                <Footer14 />
+              </section>
+              <section className="d-sm-block d-md-none bg-dark pt-5  ">
+                <div className="MobileFooter">
+                  <MobileFooter2/>
+                </div>
+              </section>
+
     </div>
   )
 }
