9b0607c205f1ace66c0bc1f48573532a8389d719
Saad Edroos <saadedroossaad@gmail.com>
2025-08-27 12:54:35 +0400

    New launch label

--- components/common/features/NewLaunchGrid.jsx
+++ components/common/features/NewLaunchGrid.jsx
@@ -151,14 +151,41 @@ const filteredProducts = products
                   ))}
                 </Swiper>
 
-                {elm.label_name && (
+                {/* {elm.label_name && (
                   <div
                     style={{ backgroundColor: elm.label_color }}
                     className="product-label text-uppercase text-white top-0 left-auto right-0 mt-2 mx-2"
                   >
                     {elm.label_name}
                   </div>
-                )}
+                )} */}
+                {Array.isArray(elm.labels) && elm.labels.length > 0 && (
+                                            <div
+                                              className="d-flex flex-column position-absolute top-0 end-0 mt-2 me-2"
+                                              style={{ gap: "4px" }}
+                                            >
+                                              {elm.labels.map((lbl, idx) => (
+                                                <LabelIcon
+                                                  key={idx}
+                                                  name={lbl.label_name}
+                                                  title={lbl.label_name}
+                                                  icon={lbl.label_color}
+                                                  size={50}
+                                                />
+                                              ))}
+                                            </div>
+                                          )}
+                                          
+                                          {/* Legacy single label */}
+                                          {!Array.isArray(elm.labels) && elm.label_name && (
+                                            <div className="position-absolute top-0 end-0 mt-2 me-2">
+                                              <LabelIcon
+                                                name={elm.label_name}
+                                                title={elm.label_name}
+                                                size={50}
+                                              />
+                                            </div>
+                                          )}
 
                 {elm.product_qty <= 0 ? (
                   <div style={{ backgroundColor: "#dc3545" }} className="product-label text-uppercase text-white top-0 left-0 mt-2 mx-2 ">
