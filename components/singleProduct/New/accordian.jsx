ecf1990e2a92477987517ded45c045ded2068ddb
Saad Edroos <saadedroossaad@gmail.com>
2025-09-22 14:55:24 +0400

    Added Delivery time

--- components/singleProduct/New/accordian.jsx
+++ components/singleProduct/New/accordian.jsx
@@ -257,6 +257,11 @@ const ProductAccordion = ({ product }) => {
             text: "Easily track your order status online anytime.",
             icon: "https://www.svgrepo.com/show/231101/placeholder-pin.svg",
         },
+        {
+            head: "Expected Delivery",
+            text: `Your order is Expected to arrive within 3-5 working days.`,
+            icon: "https://www.svgrepo.com/show/301771/delivery.svg",
+        },
     ].filter((item) => item.text);
 
     const usageInfo = getUsageInfo(product);
