15140f91dc401f456d3f00b81fa827b3ddf346f3
saood ghare <saoodghare0809@gmail.com>
2025-12-24 10:46:50 +0400

    Update DiscountGrid title to reflect current sale information

--- components/campagin/Citywalk.jsx
+++ components/campagin/Citywalk.jsx
@@ -152,7 +152,7 @@ function CityWalk() {
             </div>
         <div className="mt-3">
 
-        <DiscountGrid title="Enjoy our biggest Sale of the year" onlyDiscounted={true}/>
+        <DiscountGrid title={saleDesktop?.title} onlyDiscounted={true}/>
         </div>
             {/* <section className="d-flex section-3">
                 <div className="">
